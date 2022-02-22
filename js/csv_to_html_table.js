e0c6059122c93d3dfca0dfb8ec16eefb045e6a69
human-404 <73029415+human-404@users.noreply.github.com>
2022-02-22 21:07:38 +0800

    updated tables and reformatted other pages

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -10,6 +10,36 @@ function shuffle(array) {
     return array;
 }
 
+function tableDeleteRow(element) {
+    var rowNumberElement = (((element.parentElement).previousSibling).previousSibling).previousSibling;
+    var rowIdx = rowNumberElement.innerHTML;
+    var table = document.getElementById("table-container-table");
+    table.deleteRow(rowIdx);
+    updateSequence();
+}
+
+function tableAddRow(element) {
+    var rowNumberElement = (((element.parentElement).previousSibling).previousSibling).previousSibling;
+    var rowIdx = rowNumberElement.innerHTML;
+    var table = document.getElementById("table-container-table");
+    var newRow = table.insertRow(parseInt(rowIdx) + 1);
+    newRow.setAttribute("contenteditable", "true");
+    for (let i = 0; i < 3; i ++) {
+        newRow.insertCell(i);
+    }
+    var $controlPanel = newRow.insertCell(3);
+    $controlPanel.innerHTML = "<button class='control-panel-button' id='delButton' onclick='tableDeleteRow(this)' contenteditable='false'><img src='assets/bin.png'/></button><button class='control-panel-button' id='addButton' onclick='tableAddRow(this)' contenteditable='false'><img src='assets/plus.png'/></button>";
+    updateSequence();
+}
+
+function updateSequence() {
+    var idTable = document.getElementById("table-container-table");
+    for (var i = 1; i < idTable.rows.length; i++) {
+        var td = idTable.rows[i].cells[0];
+        td.innerHTML = i;
+    }
+}
+
 function prepend(value, array) {
     var newArray = array.slice();
     newArray.unshift(value);
@@ -22,6 +52,7 @@ CsvToHtmlTable = {
         var csv_path = options.csv_path || "";
         var el = options.element || "table-container";
         var allow_download = options.allow_download || false;
+        var write = options.write || false; // content editable
         var csv_options = options.csv_options || {};
         var datatables_options = options.datatables_options || {};
         var custom_formatting = options.custom_formatting || [];
@@ -41,7 +72,9 @@ CsvToHtmlTable = {
                 var csvData = $.csv.toArrays(data, csv_options);
                 var $tableHead = $("<thead></thead>");
                 var csvHeaderRow = prepend('#', csvData[0]);
-                console.log(csvHeaderRow);
+                if (write) {
+                    csvHeaderRow.push('Control Panel');
+                }
                 var $tableHeadRow = $("<tr></tr>");
                 for (var headerIdx = 0; headerIdx < csvHeaderRow.length; headerIdx++) {
                     $tableHeadRow.append($("<th></th>").text(csvHeaderRow[headerIdx]));
@@ -56,20 +89,33 @@ CsvToHtmlTable = {
 
                 for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
                     var $tableBodyRow = $("<tr></tr>");
-                    for (var colIdx = -1; colIdx < csvData[rowIdx].length; colIdx++) {
-                        var $tableBodyRowTd = $("<td></td>");
+                    for (var colIdx = -1; colIdx < csvHeaderRow.length - 1; colIdx++) {
+                        var $tableBodyRowTd = (write) ? $("<td contenteditable='true'></td>") : $("<td></td>");
                         var cellTemplateFunc = customTemplates[colIdx];
                         if (cellTemplateFunc) {
                             $tableBodyRowTd.html(cellTemplateFunc(csvData[rowIdx][colIdx]));
                         } else {
-                            if (colIdx == 1) {
+                            if (colIdx == -1) {
+                                if (write) {
+                                    $tableBodyRowTd.text(rowIdx); // editing enabled
+                                } else {
+                                    $tableBodyRowTd.text(shuffleOrder[rowIdx - 1]); // randomize the order 
+                                }
+                            } else if (colIdx == 1 && !write) {
                                 var list = (csvData[rowIdx][colIdx]).split(", ");
                                 for (var element of list) {
-                                    var $button = $("<button class = 'chip'>" + element + "</button>");
+                                    var $button = $("<button class='chip'>" + element + "</button>");
                                     $tableBodyRowTd.append($button);
                                 }
-                            } else if (colIdx == -1) {
-                                $tableBodyRowTd.text(shuffleOrder[rowIdx - 1]);
+                            } else if (colIdx == 0 && write) {
+                                var LaTeXText = csvData[rowIdx][colIdx];
+                                var substring = LaTeXText.substring(2, LaTeXText.length - 2);
+                                $tableBodyRowTd.text(substring);
+                            } else if (colIdx == 2 && write) {
+                                var $delButton = $("<button class='control-panel-button' id='delButton' onclick='tableDeleteRow(this)' contenteditable='false'><img src='assets/bin.png'/></button>");
+                                var $addButton = $("<button class='control-panel-button' id='addButton' onclick='tableAddRow(this)' contenteditable='false'><img src='assets/plus.png'/></button>");
+                                $tableBodyRowTd.append($delButton);
+                                $tableBodyRowTd.append($addButton);
                             } else {
                                 $tableBodyRowTd.text(csvData[rowIdx][colIdx]);
                             }
@@ -79,6 +125,10 @@ CsvToHtmlTable = {
                     }
                 }
                 $table.append($tableBody);
+
+                if (write) {
+                    $table.append("<colgroup><col span='1' style='width: 5%;'><col span='1' style='width: 50%;'><col span='1' style='width: 37.5%;'></colgroup><col span='1' style='width: 7.5%;'></colgroup>");
+                }
 
                 $table.DataTable(datatables_options);
 
