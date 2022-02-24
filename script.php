4c032baf0f5be07f96a3b05ad2ddcbfba04a9d6a
human-404 <73029415+human-404@users.noreply.github.com>
2022-02-25 00:13:30 +0800

    added auto-refresh

--- script.php
+++ script.php
@@ -3,22 +3,25 @@
     ini_set('display_errors', 1); 
     ini_set('display_startup_errors', 1); 
     error_reporting(E_ALL);
-    echo $_POST["table"];
     $table = json_decode($_POST["table"]); // table as list
 
-    $filename = "data.csv";
-    if (file_exists($filename)) {
-        $file = fopen("data.csv", "w");
-
-        // update the csv file
-        foreach ($table as $line) {
-            $a = array();
-            array_push($a, html_entity_decode($line[0]), html_entity_decode($line[1]));
-            fputcsv($file, $a);
+    if (is_null($table) || sizeof($table) == 0) {} 
+    else {
+        $filename = "data.csv";
+        if (file_exists($filename)) {
+            $file = fopen("data.csv", "w");
+    
+            // update the csv file
+            foreach ($table as $line) {
+                $a = array();
+                array_push($a, html_entity_decode($line[0]), html_entity_decode($line[1]));
+                fputcsv($file, $a);
+            }
+    
+            fclose($file);
+            header("Refresh:0; url=editor.html");
+        } else {
+            echo "the file is not present";
         }
-
-        fclose($file);
-    } else {
-        echo "the file is not present";
     }
 ?>
